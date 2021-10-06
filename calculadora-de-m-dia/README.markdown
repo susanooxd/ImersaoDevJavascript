12f742d0a0033e5ee2424fe8a5e836eccaa189d9
Jorge <86664565+susanooxd@users.noreply.github.com>
2021-10-06 13:17:17 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/README.markdown
@@ -0,0 +1,5 @@
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/zsusanoo/pen/OJggezE](https://codepen.io/zsusanoo/pen/OJggezE).
+
+
---
+++ calculadora-de-m-dia/src/script.js
@@ -0,0 +1,14 @@
+var nome = "Jorge"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundooBimestre = 10
+var notaDoTerceiroBimestre = 8
+var notaDoQuartoBimestre = 6
+
+
+var notaFixada = notaFinal.toFixed(0)
+
+console.log("bem vindo" +  nome)
+console.log(notaFixada)
+
+  // Revisão
+// variáveis, strings, console.log, toFixed, operaçõs matemáticas, concatenação, 
